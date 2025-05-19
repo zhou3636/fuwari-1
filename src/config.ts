4c8edc2cb05bd35b0a831ee448ccafa64f4c41b3
zhou3636 <zhou3534@gmail.com>
2025-05-20 00:58:28 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "ikun",
-	subtitle: "食不食油饼",
+	title: "食不食油饼",
+	subtitle: "ikun",
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -30,11 +30,11 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		{
+		  src: 'https://rr.855955.xyz/kun/kun.ico',    // Path of the favicon, relative to the /public directory
+		  theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		  sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		}
 	],
 };
 
@@ -55,13 +55,7 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/k.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "ikun真爱粉",
 	bio: "练习两年半，喜欢唱跳rap篮球。",
-	links: [
-		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
-		},
-	],
+	links: [],
 };
 
 export const licenseConfig: LicenseConfig = {
